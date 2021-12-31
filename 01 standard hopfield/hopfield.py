8dee6fb50b421b1090395b2f0ee08e67fcbc40c3
stefanbielmeier <stefanbielmeier@icloud.com>
2021-12-30 21:16:10 -0800

    made associative memory restoration work with T and H

--- 01 standard hopfield/hopfield.py
+++ 01 standard hopfield/hopfield.py
@@ -1,24 +1,21 @@
 import sys
 import os
 import csv
-import numpy as np
+import math
 
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib.cm as cm
 
 class HopfieldNetwork:
     def __init__(self, neurons):
         self.neurons = neurons
         
-        #initialize activity as random
-        a = np.random.randint(2, size=self.neurons) 
-        a[a == 0] = -1
-        self.activity = a
+        #initialize excitation of neurons as 0
+        self.excitation = np.zeros(self.neurons) 
         
-        #initialize symmetric matrix with random binary weights, and zeros
-        matrix = np.random.randint(2, size=(self.neurons,self.neurons))
-        matrix[matrix == 0] = -1
-        matrix = np.tril(matrix) + np.tril(matrix,-1).T
-        np.fill_diagonal(matrix, 0, wrap=False)
-        self.weights = matrix
+        #initialize symmetric matrix with 0 weights
+        self.weights = np.zeros((self.neurons,self.neurons))
 
         self.__updateenergy__()
 
@@ -27,19 +24,18 @@ class HopfieldNetwork:
         for i in range(self.weights.shape[0]):
             for j in range(self.weights.shape[1]):
                 if i != j:
-                    energy = energy + self.weights[i, j] * self.activity[i] * self.activity[j]
+                    energy = energy + self.weights[i, j] * self.excitation[i] * self.excitation[j]
         self.energy = energy * (-1/2)
 
     #async update, one neuron after the other, binary values only
-    def __update__(self):
-        for idx in range(self.activity.shape[0]):
-            activation = np.dot(self.weights[idx,:], self.activity)
+    def update(self, state):
+        for idx in range(state.shape[0]):
+            activation = np.dot(self.weights[idx,:], state)
             if activation >= 0:
-                self.activity[idx] = 1
+                self.excitation[idx] = 1
             else:
-                self.activity[idx] = -1
+                self.excitation[idx] = -1
         self.__updateenergy__()
-        return self.activity
 
     def learn(self, memories):
         for idx in range(self.weights.shape[0]):
@@ -49,8 +45,7 @@ class HopfieldNetwork:
                     for mem in range(memories.shape[0]):
                         result = result + memories[mem,idx] * memories[mem,jdex]
                     self.weights[idx, jdex] = result / memories.shape[0]
-        self.__update__()
-
+    
     def predict(self, partialpattern):
         result = np.zeros(partialpattern.shape[0])
         for idx in range(partialpattern.shape[0]):
@@ -61,29 +56,56 @@ class HopfieldNetwork:
                 result[idx] = -1
         return result 
 
-    def __str__(self):
-        return "State: " + str(self.activity) + "\n energy in network: " + str(self.energy)
+    def plot(self):
+        dims = int(math.sqrt(self.neurons))
+        state = self.excitation.reshape(dims,dims)
+
+        plt.figure(figsize=(dims, dims))
+        w_mat = plt.imshow(state, cmap=cm.coolwarm)
+        plt.colorbar(w_mat)
+        plt.title("Excitation of Network")
+        plt.tight_layout()
+        plt.show()
+
+
+def plot_img(img, dim):
+    plt.figure(figsize=(dim, dim))
+    w_mat = plt.imshow(img, cmap=cm.coolwarm)
+    plt.colorbar(w_mat)
+    plt.title("Img")
+    plt.tight_layout()
+    plt.show()
 
 def main():
     #capacity to store memories in Hopfield net is ~0.138 * neurons (n/2*log2())
     #For XOR / 4 memories with 3 features
     xor = np.array([[1,1,-1]])
 
-    x = np.array([[[-1,-1,-1,-1,-1],[-1,-1,-1,-1,-1],[-1,-1,-1,-1,-1],[1,1,1,1,1],[-1,-1,-1,-1,-1]], 
-    [[1,1,1,1,1],[-1,-1,-1,-1,-1],[1,1,1,1,1],[-1,-1,-1,-1,-1],[1,1,1,1,1]]])
+    x = np.array([[[1,1,1,1,1],[-1,-1,1,-1,-1],[-1,-1,1,-1,-1],[-1,-1,1,-1,-1],[-1,-1,1,-1,-1]], 
+    [[1,-1,-1,-1,1],[1,-1,-1,-1,1],[1,1,1,1,1],[1,-1,-1,-1,1],[1,-1,-1,-1,1]]])
     
     #flattens all dimensions except first dimension
     x = x.reshape(x.shape[0], -1)
 
-    partialpattern = np.array([[1,1,1,1,1],[1,1,1,1,1],[1,1,1,1,1],[-1,-1,-1,-1,-1],[1,1,1,1,1]]).flatten()
+    #three noise bits
+    noisy_t = np.array([[1,-1,1,1,1],[-1,-1,1,-1,1],[-1,1,1,-1,-1],[-1,-1,1,-1,1],[-1,-1,1,-1,-1]])
+
+    noisy_h = np.array([[-1,-1,-1,-1,1],[1,1,-1,-1,1],[1,1,1,1,1],[1,-1,-1,-1,1],[1,1,1,-1,1]])
 
     network = HopfieldNetwork(25)
-    print(network)
+    #network.plot()
     network.learn(x)
-    #sign blind
-    print(network)
+    network.update(x[0].flatten())
 
-    print(network.predict(partialpattern))
+    plot_img(noisy_t, 5)
+    network.update(noisy_t.flatten())
+    network.plot()
+
+    #sort of doesn't work yet – probably because it updates excitation
+    plot_img(noisy_h, 5)
+    network.update(noisy_h.flatten())
+    network.plot()
+
 
 if __name__ == "__main__":
     main()
