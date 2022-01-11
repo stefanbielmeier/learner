ceb8949e86fb75f1d09066ad675c2e2ecacad17f
stefanbielmeier <stefanbielmeier@icloud.com>
2022-01-11 15:59:13 -0500

    calc max_capacity

--- 02 densehopfield/maxcapacity.py
+++ 02 densehopfield/maxcapacity.py
@@ -10,7 +10,7 @@ n = degree of the polynomial smoothing function in the update function of the ne
 import math
 
 from scipy.special import factorial2
-import matplotlib as plt
+import matplotlib.pyplot as plt
 
 #calc capacity
 def calc_k (num_neurons, poly_degree):
@@ -21,6 +21,17 @@ def calc_k (num_neurons, poly_degree):
 
     return one*two
 
-print(calc_k(25,2))
+def plot_graph(start_neurons, end_neurons):
+    degrees = range(1,50) #polynomial degree of function
 
+    for neuron in range(start_neurons, end_neurons+1):
+        y_values = []
+        for degree in degrees:
+            y_values.append(calc_k(neuron, degree))
+
+        plt.plot(degrees, y_values)
+    plt.show()
+
+
+plot_graph(40,45)
 
