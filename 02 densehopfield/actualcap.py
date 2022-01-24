ed3f5d5478b0a9f8c1493b89849670fc6ded4455
stefanbielmeier <stefanbielmeier@icloud.com>
2022-01-25 00:22:05 +0100

    duplicated data test

--- 02 densehopfield/actualcap.py
+++ 02 densehopfield/actualcap.py
@@ -43,42 +43,47 @@ def get_recall_qualities(memories, polydegrees, num_neurons, network_max_cap = F
 
     return recall_qualities
 
-for examples_per_class in num_examples:
-#3 create random memories
-    num_classes = int(total_memories/examples_per_class)
-    random = np.random.randint(0,2,num_classes*num_neurons)
-    randomarray = np.where(random == 0, -1, random)
+def main():
 
-    #extend the random data #num_examples of times
-    random_ints = np.tile(randomarray, examples_per_class)
+    for examples_per_class in num_examples:
+    #3 create random memories
+        num_classes = int(total_memories/examples_per_class)
+        random = np.random.randint(0,2,num_classes*num_neurons)
+        randomarray = np.where(random == 0, -1, random)
 
-    #store as float, as well
-    random_floats = np.array(random_ints, dtype=np.float64)
+        #extend the random data #num_examples of times
+        random_ints = np.tile(randomarray, examples_per_class)
 
-    #constant memories
-    #constantarray = np.random.randint(1,2,num_unique_memories*num_neurons)
+        #store as float, as well
+        random_floats = np.array(random_ints, dtype=np.float64)
 
-    #int_memories = np.reshape(random_ints,(num_examples*num_unique_memories,num_neurons))
-    float_memories = np.reshape(random_floats,(total_memories,num_neurons))
-    #int_constant_memories = np.reshape(constantarray, (num_unique_memories, num_neurons))
-    #float_constant_memories = np.array(int_constant_memories, dtype=np.float64)
+        #constant memories
+        #constantarray = np.random.randint(1,2,num_unique_memories*num_neurons)
 
-    #print("int", int_memories)
-    print("float", float_memories)
-    #print("const int", int_constant_memories)
-    #print("const float", float_constant_memories)
+        #int_memories = np.reshape(random_ints,(num_examples*num_unique_memories,num_neurons))
+        float_memories = np.reshape(random_floats,(total_memories,num_neurons))
+        #int_constant_memories = np.reshape(constantarray, (num_unique_memories, num_neurons))
+        #float_constant_memories = np.array(int_constant_memories, dtype=np.float64)
 
-    #for plot
-    polydegrees = np.arange(1,max_polynomial) #x
+        #print("int", int_memories)
+        print("float", float_memories)
+        #print("const int", int_constant_memories)
+        #print("const float", float_constant_memories)
 
-    #ints_recall_qualities = get_recall_qualities(int_memories, polydegrees, num_neurons)
-    float_recall_qualities = get_recall_qualities(float_memories, polydegrees, num_neurons)
-#   constant_int_recall_qualities = get_recall_qualities(int_constant_memories, polydegrees, num_neurons)
-#   constant_float_recall_qualities = get_recall_qualities(float_constant_memories, polydegrees, num_neurons)
+        #for plot
+        polydegrees = np.arange(1,max_polynomial) #x
 
-    plt.plot(polydegrees, float_recall_qualities, label="examples per class: {}".format(examples_per_class))
+        #ints_recall_qualities = get_recall_qualities(int_memories, polydegrees, num_neurons)
+        float_recall_qualities = get_recall_qualities(float_memories, polydegrees, num_neurons)
+    #   constant_int_recall_qualities = get_recall_qualities(int_constant_memories, polydegrees, num_neurons)
+    #   constant_float_recall_qualities = get_recall_qualities(float_constant_memories, polydegrees, num_neurons)
 
-plt.legend(loc='best')
-plt.show()
+        plt.plot(polydegrees, float_recall_qualities, label="examples per class: {}".format(examples_per_class))
 
-#plt.plot(polydegrees, constant_int_recall_qualities, constant_float_recall_qualities)
+    plt.legend(loc='best')
+    plt.show()
+
+    #plt.plot(polydegrees, constant_int_recall_qualities, constant_float_recall_qualities)
+
+if __name__ == "__main__":
+    main()
--- 02 densehopfield/duplicatingdata.py
+++ 02 densehopfield/duplicatingdata.py
@@ -1,4 +1,6 @@
 import numpy as np
+from actualcap import get_recall_qualities
+import matplotlib.pyplot as plt
 
 num_classes = 2
 num_neurons = 16
@@ -6,9 +8,26 @@ num_neurons = 16
 random = np.random.randint(0,2,16_000) 
 randomarray = np.array(np.where(random == 0, -1, random), dtype=np.float64)
 
-data = np.reshape(randomarray, (1_000,16))
+data = np.reshape(randomarray, (1_000,16)) 
 
-duplicateddata = np.tile(data, 2)
+duplicateddata = np.tile(data, (4,1))
 
-print(data.shape)
-print(duplicateddata.shape)
+#test 1: train Hopfield Network on 1_000 random memories, and compare to duplicated 2_000 memories without noise
+
+polydegrees = np.arange(1,20) #m
+recall_qualities_1000 = get_recall_qualities(data, polydegrees, num_neurons)
+recall_qualities_duplicated = get_recall_qualities(duplicateddata, polydegrees, num_neurons)
+plt.plot(polydegrees, recall_qualities_1000, label="dataset with 1000 examples")
+plt.plot(polydegrees, recall_qualities_duplicated, label="duplicated 1000 dataset, total of 2000 examples")
+
+#test 2: train Hopfield network on 1_000 random memories and compare to duplicated 2_000 memories with noise
+
+arr = np.array([0] * 15950 + [1] * 25 + [-1] *25) # creates 50 bits of noise for 16000 bits of data
+np.random.shuffle(arr) #shuffle noisy bits
+noisydata = np.where(arr == 0, randomarray, arr)
+
+noisyduplicateddata = np.reshape(np.stack((randomarray, noisydata, noisydata, noisydata), axis=0), (4_000,16))
+recall_qualities_noisyduplicate = get_recall_qualities(noisyduplicateddata, polydegrees, num_neurons)
+plt.plot(polydegrees, recall_qualities_noisyduplicate, label="noisy duplicate")
+plt.legend(loc="best")
+plt.show()
