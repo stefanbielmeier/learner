80896fc6d271c72fdd7cf6206acb8e52a0dff835
stefanbielmeier <stefanbielmeier@icloud.com>
2022-01-25 14:04:12 +0100

    4000 examples duplicates

--- 02 densehopfield/duplicatingdata.py
+++ 02 densehopfield/duplicatingdata.py
@@ -14,19 +14,20 @@ duplicateddata = np.tile(data, (4,1))
 
 #test 1: train Hopfield Network on 1_000 random memories, and compare to duplicated 2_000 memories without noise
 
-polydegrees = np.arange(1,20) #m
+polydegrees = np.arange(1,30) #m
 recall_qualities_1000 = get_recall_qualities(data, polydegrees, num_neurons)
 recall_qualities_duplicated = get_recall_qualities(duplicateddata, polydegrees, num_neurons)
 plt.plot(polydegrees, recall_qualities_1000, label="dataset with 1000 examples")
-plt.plot(polydegrees, recall_qualities_duplicated, label="duplicated 1000 dataset, total of 2000 examples")
+plt.plot(polydegrees, recall_qualities_duplicated, label="quadrupeled 1000 dataset, total of 4000 examples")
 
 #test 2: train Hopfield network on 1_000 random memories and compare to duplicated 2_000 memories with noise
 
-arr = np.array([0] * 15950 + [1] * 25 + [-1] *25) # creates 50 bits of noise for 16000 bits of data
+arr = np.array([0.] * 47000 + [1.] * 500 + [-1.] *500) # creates 1000 bits of noise for 48000 bits of data
 np.random.shuffle(arr) #shuffle noisy bits
-noisydata = np.where(arr == 0, randomarray, arr)
+d3_000 = np.tile(randomarray, 3)
+noisydata = np.where(arr == 0, d3_000, arr)
 
-noisyduplicateddata = np.reshape(np.stack((randomarray, noisydata, noisydata, noisydata), axis=0), (4_000,16))
+noisyduplicateddata = np.reshape(np.concatenate((randomarray, noisydata)), (4_000,16))
 recall_qualities_noisyduplicate = get_recall_qualities(noisyduplicateddata, polydegrees, num_neurons)
 plt.plot(polydegrees, recall_qualities_noisyduplicate, label="noisy duplicate")
 plt.legend(loc="best")
