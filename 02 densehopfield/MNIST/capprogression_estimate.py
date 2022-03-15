74e657107d8ede83fd1be4554d5d8f8ab0b11faa
stefanbielmeier <stefanbielmeier@icloud.com>
2022-03-15 14:07:58 +0100

    finish cap progression experiment with VC dimension plot

--- 02 densehopfield/MNIST/capprogression_estimate.py
+++ 02 densehopfield/MNIST/capprogression_estimate.py
@@ -1,12 +1,8 @@
-from email.mime import base
-import torchvision
-import numpy as np
 import matplotlib.pyplot as plt
+import numpy as np
+import torchvision
 
 from scipy.stats import entropy
-
-from minmemorypoly import get_memorization_capacity
-from estimate_cap import estimate_cap
 
 num_neurons = 784
 num_memories = 100
@@ -47,19 +43,10 @@ train_binary_subset = np.array(np.where(train_subset >= 128, 1, -1), dtype=np.fl
 selected_0s = np.take(train_binary_subset, range(0,50), axis=0)
 selected_1s = np.take(train_binary_subset, range(6000,6050), axis=0)
 
-#x percentage of the dataset analyzed
 dataset_share = np.array([0.02, 0.04, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1]) #0.04 is 2 images in each "class"
+x_axis = dataset_share * 100
 
-#uniform random dataset
-uniform_random_binary = np.reshape(np.array(np.random.randint(0, 2, num_neurons*num_memories), dtype=np.float64), (num_memories, num_neurons))  # 100 memories random dataset
-uniform_random_unary = np.array(np.where(uniform_random_binary == 0, -1, uniform_random_binary), dtype=np.float64)
-
-p_uniform_random = np.sum(uniform_random_binary, axis=0)
-
-min_memorization_polydegrees = []
-uniform_random_thresholds = []
-zero_one_thresholds = []
-kl_divergence = []
+information = []
 
 for share in dataset_share:
     zero_subset = selected_0s[0:int(share*50), :]
@@ -68,48 +55,43 @@ for share in dataset_share:
     partial_zero_ones_unary = np.concatenate((zero_subset, one_subset))
     partial_zero_ones_binary = np.array(np.where(partial_zero_ones_unary == -1, 0, partial_zero_ones_unary), dtype=np.float64)
 
-    memorization_polydegree = get_memorization_capacity(partial_zero_ones_unary)
-    
-    partial_uniform_random_binary = uniform_random_binary[0:int(share*num_memories),:] #2, 4, 10, 20 etc. memories
-    partial_uniform_random_unary = uniform_random_unary[0:int(share*num_memories),:] #2, 4, 10, 20 etc. memories
+    p_zero_ones = np.sum(partial_zero_ones_binary, axis=0)
 
-    
-    #threshold estimation of -1 and 1 data
-    zero_one_cap, _ = estimate_cap(partial_zero_ones_unary)
-    zero_one_thresholds.append(zero_one_cap)
+    info_content = entropy(p_zero_ones, base=2)
+    information.append(info_content)
 
-    uniform_thresholds, _ = estimate_cap(partial_uniform_random_unary)
-    uniform_random_thresholds.append(uniform_thresholds)
-    
-    #KL divergence from uniform random data, measured in bits of information
-    #get probability distribution for Data
-    p_zero_one = np.sum(partial_zero_ones_binary, axis=0)
+mem_cap = np.array([2, 4, 8, 12, 16, 20, 21, 25, 24, 26, 29, 27])
+info_gain = np.array([1.8828204158228377, 1.6758262676616411, 1.4892695268950995, 1.4081247313418512, 1.4022956608563981, 1.4205585227111361, 1.4130696261585378, 1.3961868620537718, 1.380697627864883, 1.3682492575455916, 1.3730425461464963, 1.3741251006585482])
+random_thresholds = np.array([1.0, 2.0, 2.807354922057604, 3.584962500721156, 4.169925001442312, 4.754887502163468, 5.0, 5.129283016944966, 5.247927513443585, 5.357552004618084, 5.491853096329675, 5.614709844115208])
+zero_one_thresholds = np.array([1.0, 2.0, 3.321928094887362, 4.247927513443585, 4.754887502163468, 5.129283016944966, 5.459431618637297, 5.700439718141092, 5.857980995127572, 6.0, 6.129283016944966, 6.266786540694901])
 
-    kl_divergence.append(entropy(p_zero_one, p_uniform_random, base=2)) #P: zeros and ones, Q (reference): uniform random
 
-    min_memorization_polydegrees.append(memorization_polydegree)
+#Print information
+print(zero_one_thresholds*6)
+print(mem_cap)
 
-#Goal: FOR EACH SHARE, plot #thresholds in uniform random data, #thresholds in actual data
+
+##plotting the stuff again
 
 fig, ax = plt.subplots()
 
 x_axis = dataset_share * 100
 
-print(min_memorization_polydegrees)
-print(kl_divergence)
-print(uniform_random_thresholds)
-print(zero_one_thresholds)
-
-ax.plot(x_axis, min_memorization_polydegrees)
+ax.plot(x_axis, mem_cap, label='mem_cap', color='cyan')
 ax.set_xlabel("number of memories")
 ax.set_ylabel("memorization capacity")
-ax.legend(loc='best')
+ax.legend()
 
 ax2 = ax.twinx()
-ax2.plot(x_axis, kl_divergence, label='KL divergence')
-ax2.plot(x_axis, uniform_random_thresholds, label='Uniform Random Thresholds')
+#ax2.plot(x_axis, info_gain, label='KL divergence')
+ax2.plot(x_axis, random_thresholds, label='Uniform Random Thresholds')
 ax2.plot(x_axis, zero_one_thresholds, label='Zero and One Thresholds')
+
 ax2.set_ylabel("Thresholds and KL Divergence in bits")
 ax2.legend()
 
 plt.show()
+
+
+
+
