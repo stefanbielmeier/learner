68adc6856dd0fd0b6f1f575b2fad0a40df401304
stefanbielmeier <stefanbielmeier@icloud.com>
2022-04-20 23:42:47 +0200

    fix bug

--- root/experiments/XOR/scaling.py
+++ root/experiments/XOR/scaling.py
@@ -9,7 +9,7 @@ import numpy as np
 def calc_c(num_memories, num_neurons, poly_degree):
     log = np.log(num_memories)/np.log(num_neurons)
     exponent = log-poly_degree+1
-    c = num_memories**exponent
+    c = (num_neurons**exponent)
     return c
 
 
