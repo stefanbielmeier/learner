f65b276800df043144d3b5871887f9d538d857ee
stefanbielmeier <stefanbielmeier@icloud.com>
2022-05-11 16:07:30 +0200

    add plot function

---
+++ root/experiments/MNIST/plot/capacity_progression.py
@@ -0,0 +1,22 @@
+import matplotlib.pyplot as plt
+
+from root.experiments.MNIST.capacity.definitions import X_AXIS
+from root.experiments.MNIST.capacity.results import LOG2_THRESHOLDS_SIX_EIGHT, LOG2_THRESHOLDS_ZERO_ONE, RANDOM_DATA_LOG2_TRESHOLDS, MEM_CAPS_0and1, MEM_CAPS_6and8
+
+
+def plot_progression(title, x_data, y_data, x_label):
+
+    fig, ax = plt.subplots()
+    ax.set_xlabel(x_label)
+    
+    for y_axis in y_data:
+        print(y_axis)
+        ax.plot(x_data, y_axis, label='mem cap')
+
+    ax.set_ylim(0, 30)
+    ax.legend(loc='upper left')
+    fig.suptitle(title)
+
+    plt.show()
+
+plot_progression("Memorization capacity and number of thresholds", X_AXIS, [MEM_CAPS_0and1, MEM_CAPS_6and8, LOG2_THRESHOLDS_ZERO_ONE, LOG2_THRESHOLDS_SIX_EIGHT, RANDOM_DATA_LOG2_TRESHOLDS], "Memorization Capacity and Number of Thresholds")
