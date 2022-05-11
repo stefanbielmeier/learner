58465296d7e1d94faaa49493f5e888f9ecb9b167
stefanbielmeier <stefanbielmeier@icloud.com>
2022-05-11 13:52:21 +0200

    refactored progression metrics

---
+++ root/experiments/MNIST/capacity/definitions.py
@@ -0,0 +1,4 @@
+import numpy as np
+
+DATASET_SHARE = np.array([0.02, 0.04, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1])
+X_AXIS = DATASET_SHARE * 100
---
+++ root/experiments/MNIST/capacity/progression_metrics.py
@@ -0,0 +1,48 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+from root.infocapacity.estimate_cap import estimate_cap
+
+from root.experiments.MNIST.digits.subsets import get_first_fifty_images
+
+from root.experiments.MNIST.capacity.definitions import DATASET_SHARE
+
+num_neurons = 784
+num_memories = 100
+
+def calc_thresholds(first_set, second_set = None):
+    log_thresholds = []
+
+    for share in DATASET_SHARE:
+        first_subset = first_set[0:int(share*50), :]
+        
+        if second_set is not None:
+            second_subset = second_set[0:int(share*50), :]
+            set = np.concatenate((first_subset, second_subset))
+        else:
+            set = first_subset
+            
+        log_threshold, _ = estimate_cap(set)
+
+        log_thresholds.append(log_threshold)
+
+    log_thresholds = np.array(log_thresholds, dtype=np.float64)
+    
+    return log_thresholds
+
+def calc_prob_density():
+    #TODO
+
+    zeros, ones, sixes, eights = get_first_fifty_images(inBinary = True)
+    
+    #binary_set = np.array(np.where(unary_set == -1, 0, unary_set), dtype=np.float64)
+
+    #p_six_eights = np.sum(binary_set, axis=0)
+    pass
+
+def main():
+    zeros, ones, sixes, eights = get_first_fifty_images(inBinary = True)
+    calc_thresholds(sixes, eights)
+
+if __name__ == "__main__":
+    main()
