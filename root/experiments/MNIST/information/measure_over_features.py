b2a080597eb66fa89e4a792bb7b7e723da5fe5c0
stefanbielmeier <stefanbielmeier@icloud.com>
2022-05-26 12:03:40 +0200

    change measure_over_features again

--- root/experiments/MNIST/information/measure_over_features.py
+++ root/experiments/MNIST/information/measure_over_features.py
@@ -5,32 +5,47 @@ from root.experiments.MNIST.digits.subsets import make_binary
 from scipy.spatial.distance import jensenshannon
 from scipy.stats import entropy
 
-zeros, ones, sixes, eights = get_first_fifty_images(inBinary=False)
+from root.utils import plot_img
+
+zeros, ones, sixes, eights = get_first_fifty_images(inBinary=True)
 
 num_neurons = 784
 num_memories = 50
 
-uniform_random = np.random.randint(0, 2, num_memories)
+white = np.full(num_neurons,-1, dtype=np.float64)
 
-def calculate_bits (dataset, uniform_random):
+uniform_random = np.random.randint(0, 1, num_neurons)
+#uniform_random = np.where(uniform_random == 0, -1, 1)
+
+def calculate_bits(dataset, uniform_random):
     bit_divs = []
     for column in dataset.T:
-        bit_divs.append(jensenshannon(column, uniform_random, base=2))
+        #make a probability distribution out of the float values of column
+        _, inverse = np.unique(column, return_inverse=True)
+        p = np.bincount(inverse)
+        #make a probabiltiy distribution out of the values of column in uniform random
+        _, inverse2 = np.unique(uniform_random, return_inverse=True)
+        q = np.bincount(inverse2)
+        #print("p:", p)
+        #print("q:", q)
+        bit_divs.append(jensenshannon(p, q, base=2))
     bit_divs = np.array(bit_divs, dtype=np.float64)
     bit_divs = np.nan_to_num(bit_divs)
     return np.sum(bit_divs)
+
 
 print(calculate_bits(zeros, uniform_random))
 print(calculate_bits(ones, uniform_random))
 print(calculate_bits(sixes, uniform_random))
 print(calculate_bits(eights, uniform_random))
+print(calculate_bits(white, uniform_random))
 
 print("ensemble")
 
-uniform_random_long = np.random.randint(0, 2, num_memories*2)
+uniform_random_long = np.random.randint(0, 1, num_neurons)
 
 six_eights = np.vstack((sixes, eights))
 zero_ones = np.vstack((zeros, ones))
 
-print(calculate_bits(zero_ones, uniform_random_long))
-print(calculate_bits(six_eights, uniform_random_long))
+print("zero and ones in bits", calculate_bits(zero_ones, uniform_random_long))
+print("sixes and eights in bits", calculate_bits(six_eights, uniform_random_long))
