c1f82bcb17c36607be81dc29953aa926ebd7b363
stefanbielmeier <stefanbielmeier@icloud.com>
2022-05-30 18:22:24 +0200

    add hamming for xor

--- root/experiments/MNIST/information/error_correcting.py
+++ root/experiments/MNIST/information/error_correcting.py
@@ -3,8 +3,8 @@ from root.experiments.MNIST.digits.subsets import get_first_fifty_images
 from root.experiments.MNIST.digits.subsets import make_binary
 
 
-
-zeros, ones, sixes, eights = get_first_fifty_images(inBinary=True, zeroOnes=True)
+zeros, ones, sixes, eights = get_first_fifty_images(
+    inBinary=True, zeroOnes=True)
 
 num_neurons = 784
 num_memories = 50
@@ -12,24 +12,65 @@ num_memories = 50
 uniform_random = np.reshape(np.random.randint(
     0, 2, num_memories*num_neurons), (num_memories, num_neurons))
 
+#Hamming distance is inverse similarity. Smaller distance means more similar. Harder to learn!
+#So for hamming distance, we want to check how many bits need to be flipped to get from one
+#pattern to any other one.
+#The task of a Hopfield Network is to flip the bits in the input to get to the closest pattern.
+#What is the pattern furthest away from any input pattern but can still be restored?
+#How many bits have to be flipped to get from the noisy-most representation of a Hamming
+#That is the capacity of the Hopfield Network.
+
+
 def hamming_distance(string1, string2):
-	dist_counter = 0
-	for n in range(len(string1)):
-		if string1[n] != string2[n]:
-			dist_counter += 1
-	return dist_counter
+    # O(n) runtime
+    dist_counter = 0
+    for n in range(len(string1)):
+        if string1[n] != string2[n]:
+            dist_counter += 1
+    return dist_counter
 
 
-def calculate_bits(dataset, uniform_random):
+def calculate_bits(dataset):
+    # O(N^2) runtime with N is number of patterns in the data
     bit_divs = []
-    for row_num in range(dataset.shape[0]):
-        bit_divs.append(hamming_distance(dataset[row_num,:], uniform_random[row_num,:]))
+    for i in range(dataset.shape[0]):
+        for j in range(dataset.shape[0]):
+            if i != j:
+                bit_divs.append(hamming_distance(dataset[i, :], dataset[j, :]))
 
     bit_divs = np.array(bit_divs, dtype=np.float64)
+    return np.min(bit_divs)
 
-    return np.sum(bit_divs)
 
-print(calculate_bits(zeros, uniform_random))
-print(calculate_bits(ones, uniform_random))
-print(calculate_bits(sixes, uniform_random))
-print(calculate_bits(eights, uniform_random))
+distance0 = calculate_bits(zeros)
+distance1 = calculate_bits(ones)
+distance6 = calculate_bits(sixes)
+distance8 = calculate_bits(eights)
+
+print(distance0)
+print(distance1)
+print(distance6)
+print(distance8)
+
+zero_ones = np.vstack((zeros, ones))
+six_eights = np.vstack((sixes, eights))
+
+distance_01 = calculate_bits(zero_ones)
+distance_68 = calculate_bits(six_eights)
+distance_uni = calculate_bits(uniform_random)
+
+print(distance_01)
+print(distance_68)
+print(distance_uni)
+
+xor = np.array([[0,0,0],[0,1,1],[1,0,1],[1,1,0]])
+xand = np.array([[0,0,1],[0,1,0],[1,0,0],[1,1,1]])
+
+xand_distance = calculate_bits(xand)
+xor_distance = calculate_bits(xor)
+
+print("XOR distance", xand_distance)
+print("XAND distance", xand_distance)
+
+#19 bits is the distance difference between the two bottlenecks of the datasets 
+#the 2 most similar patterns from the dataset are the bottleneck (if we want 100% accuracy)
