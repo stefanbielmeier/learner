efe722da9f4685549d2fd3df9634872a632d6678
stefanbielmeier <stefanbielmeier@icloud.com>
2022-06-02 20:19:50 +0200

    add feature: get bottleneck pair and print it

--- root/experiments/MNIST/information/hamming.py
+++ root/experiments/MNIST/information/hamming.py
@@ -1,10 +1,14 @@
 import numpy as np
+import math
 from root.experiments.MNIST.digits.subsets import get_first_fifty_images
 from root.experiments.MNIST.digits.subsets import make_binary
+from root.utils import plot_img
 
 
 zeros, ones, sixes, eights = get_first_fifty_images(
     inBinary=True, zeroOnes=True)
+zero_ones = np.vstack((zeros, ones))
+six_eights = np.vstack((sixes, eights))
 
 num_neurons = 784
 num_memories = 50
@@ -29,52 +33,57 @@ def hamming_distance(string1, string2):
             dist_counter += 1
     return dist_counter
 
+def hamming_distances(code):
+    distances = []
+    for i in range(code.shape[0]):
+        for j in range(code.shape[0]):
+            distances.append(hamming_distance(code[i, :], code[j, :]))
+    distances = np.array(distances, dtype=np.float64)
+    return distances
 
-def hamming_distance(code):
-    # O(N^2) runtime with N is number of patterns in the data
-    dataset = code
-    bit_divs = []
-    for i in range(dataset.shape[0]):
-        for j in range(dataset.shape[0]):
-            if i != j:
-                bit_divs.append(hamming_distance(dataset[i, :], dataset[j, :]))
+def min_hamming_distance(code):
+    distances = hamming_distances(code)
+    return np.min(distances[distances != 0])
 
-    bit_divs = np.array(bit_divs, dtype=np.float64)
-    return np.min(bit_divs)
+def mean_hamming_distance(code):
+    return np.mean(hamming_distances(code))
 
-def mean_hamming_distance_in(code):
-    dataset = code
-    bit_divs = []
-    for i in range(dataset.shape[0]):
-        for j in range(dataset.shape[0]):
-            if i != j:
-                bit_divs.append(hamming_distance(dataset[i, :], dataset[j, :]))
+def get_bottleneck_pairs(code):
+    # Get the index of the first and second string pair in the dataset that are most similar according to Hamming distance (smallest)
+    distances = hamming_distances(code)
+    min_dist = min_hamming_distance(code)
+    pair_idx = 0
+    for idx in range(distances.shape[0]):
+        if distances[idx] == min_dist:
+            pair_idx = idx        
+    i = math.floor(pair_idx / code.shape[0])
+    j = pair_idx % code.shape[0]
+    return [i,j]
 
-    bit_divs = np.array(bit_divs, dtype=np.float64)
-    return np.mean(bit_divs)
+def error_correction_capability(code):
+    return (min_hamming_distance(code)-1)/2
 
-def calculate_error_correction(dataset):
-    return error_correction(hamming_distance(dataset))
+bottleneck = get_bottleneck_pairs(zero_ones)
+print(bottleneck)
+print(hamming_distance(zero_ones[bottleneck[0], :], zero_ones[bottleneck[1], :]))
+print(min_hamming_distance(zero_ones))
+plot_img(zero_ones[bottleneck[0],:].reshape(28,28), 5)
+plot_img(zero_ones[bottleneck[1],:].reshape(28,28), 5)
+"""
 
-def error_correction(hamming_distance):
-    return (hamming_distance-1)/2
-
-distance0 = hamming_distance(zeros)
-distance1 = hamming_distance(ones)
-distance6 = hamming_distance(sixes)
-distance8 = hamming_distance(eights)
+distance0 = min_hamming_distance(zeros)
+distance1 = min_hamming_distance(ones)
+distance6 = min_hamming_distance(sixes)
+distance8 = min_hamming_distance(eights)
 
 print(distance0)
 print(distance1)
 print(distance6)
 print(distance8)
 
-zero_ones = np.vstack((zeros, ones))
-six_eights = np.vstack((sixes, eights))
-
-distance_01 = hamming_distance(zero_ones)
-distance_68 = hamming_distance(six_eights)
-distance_uni = hamming_distance(uniform_random)
+distance_01 = min_hamming_distance(zero_ones)
+distance_68 = min_hamming_distance(six_eights)
+distance_uni = min_hamming_distance(uniform_random)
 
 print(distance_01)
 print(distance_68)
@@ -83,11 +92,12 @@ print(distance_uni)
 xor = np.array([[-1,-1,-1],[-1,1,1],[1,-1,1],[1,1,-1]], dtype=np.float32)
 xand = np.array([[-1,-1,1],[-1,1,-1],[1,-1,-1],[1,1,1]], dtype=np.float32)
 
-xand_distance = hamming_distance(xand)
-xor_distance = hamming_distance(xor)
+xand_distance = min_hamming_distance(xand)
+xor_distance = min_hamming_distance(xor)
 
 print("XOR distance", xand_distance)
 print("XAND distance", xand_distance)
 
 #19 bits is the distance difference between the two bottlenecks of the datasets 
 #the 2 most similar patterns from the dataset are the bottleneck (if we want 100% accuracy)
+"""
